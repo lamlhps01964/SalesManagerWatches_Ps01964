ebd7be63a00139461b562c2b37203364e7828208
lamlhps01964 <lamlhps01964@fpt.edu.vn>
2015-04-11 23:31:59 +0700

    123

---
+++ SalesManagerWatches_Ps1/Form1.vb
@@ -0,0 +1,29 @@
+﻿Public Class frmMain
+    'sự kiện menustrip thoát
+    Private Sub mniThoat_Click(sender As Object, e As EventArgs) Handles mniThoat.Click
+        End
+    End Sub
+    'sự kiện nút đăng xuất
+    Private Sub mniDangXuat_Click(sender As Object, e As EventArgs) Handles mniDangXuat.Click
+        'form Login xuất hiện
+        Me.Hide()
+       
+    End Sub
+
+    'sự kiện vào form thêm, sửa, xóa,...
+    Private Sub mniThem_Click(sender As Object, e As EventArgs) Handles mniThem.Click
+
+    End Sub
+    'sự kiện form About
+    Private Sub mniBanQuyen_Click(sender As Object, e As EventArgs) Handles mniBanQuyen.Click
+        
+    End Sub
+    'sự kiện form trợ giúp
+    Private Sub mniTroGiup_Click(sender As Object, e As EventArgs) Handles mniTroGiup.Click
+        Help.ShowHelp(Me, "Help_Assignment_PS01964.chm")
+    End Sub
+
+    Private Sub mniKH_Click(sender As Object, e As EventArgs) Handles mniKH.Click
+
+    End Sub
+End Class
